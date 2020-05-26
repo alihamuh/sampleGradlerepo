40f1068d6909b6104a382f74d4303baf2bae3989
alihamuh <alihamuh@gmail.com>
2020-05-26 07:40:59 +0500

    First Eclipse Commit

---
+++ src/main/java/testProject2/mainClass.java
@@ -0,0 +1,29 @@
+package testProject2;
+
+import java.util.Optional;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ProcessHandle.allProcesses()
+        .forEach(process -> System.out.println(processDetails(process)));
+		
+          
+	}
+	
+	private static String processDetails(ProcessHandle process) {
+	    return String.format("%8d %8s %10s %26s %-40s",
+	            process.pid(),
+	            text(process.parent().map(ProcessHandle::pid)),
+	            text(process.info().user()),
+	            text(process.info().startInstant()),
+	            text(process.info().commandLine()));
+	}
+
+	private static String text(Optional<?> optional) {
+	    return optional.map(Object::toString).orElse("-");
+	}
+
+}
